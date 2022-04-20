44dddfa9e6b65b2ac9cc3f2f4738bffed484dcb4
Ramon van der Weiden <rweiden@nomartech.nl>
2022-04-20 22:08:11 +0200

    Exercises

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+max_height = 100
+new_height = max_height
+velocity = 3 / 5
+total_bounces = 1
+
+while total_bounces < 11: 
+    new_height = new_height * velocity
+    print(f'{total_bounces} {round(new_height,4)}')
+    total_bounces = total_bounces + 1
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,33 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+months_paid = 0
+
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
+while principal > 0:
+    months_paid = months_paid + 1
+
+    if principal < payment: 
+        principal = principal - principal
+        total_paid = total_paid + principal
+    else:    
+        principal = principal * (1+rate/12) - payment
+        total_paid = total_paid + payment
+    
+    if months_paid >= extra_payment_start_month and months_paid <= extra_payment_end_month :
+        principal = principal - extra_payment
+        total_paid = total_paid + extra_payment
+    
+    print(months_paid, round(total_paid, 2), round(principal, 2))
+
+
+print('Total paid', round(total_paid, 2))
+print('Total months', months_paid)
